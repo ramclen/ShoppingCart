a70e6d6a5cc3d5060248563b93c29fc2c0f22c61
ramclen <cristopher.correo@gmail.com>
2017-09-02 23:42:01 +0100

    Improve looking on UserInformationComponent

--- src/User/UserInformationComponent.js
+++ src/User/UserInformationComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import GSpreadSheets from "../GSpreadSheets";
+import {Paper, RaisedButton, TextField} from "material-ui";
 
 export default class UserInformationComponent extends React.Component{
 
@@ -27,10 +28,13 @@ export default class UserInformationComponent extends React.Component{
 
     render(){
         return (
-            <div>
-                <input value={this.state.spreadSheet} onChange={this.setSpreadSheetChange.bind(this)} type="text" placeholder="insert your drive"/>
-                <button onClick={this.setSpreadSheet.bind(this)}>Go!</button>
-            </div>
+            <Paper zDepth={3} style={{padding:"20px 0", textAlign:"center", width:"500px", height:"200px",position: "absolute",top:0,bottom: 0,left: 0,right: 0,margin: "auto"}}>
+                <div>
+                    <img src={'http://www.learnersdictionary.com/media/ld/images/legacy_print_images/basket.gif'} alt="main"/>
+                </div>
+                <TextField floatingLabelText={"Your Drive ID"} value={this.state.spreadSheet} onChange={this.setSpreadSheetChange.bind(this)} type="text"/>
+                <RaisedButton label={"Go!"} secondary={true} onClick={this.setSpreadSheet.bind(this)} />
+            </Paper>
         )
     }
 
